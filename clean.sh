7e3b852ba315654885ff0e46271121e4899eacc5
ylf <iivodka5@gmail.com>
2019-07-22 23:15:20 +0000

    see

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 find . -name '*.log' -type f -delete
 find . -name '*.aux' -type f -delete
 find . -name '*.synctex.gz' -type f -delete
+find . -name '*.fls' -type f -delete
+find . -name '*.fdb_latexmk' -type f -delete
