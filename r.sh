7f823a3542f716650257272d7f6cdea44fa9f346
ylf <iivodka5@gmail.com>
2019-07-28 15:05:26 +0000

    c

---
+++ r.sh
@@ -0,0 +1,7 @@
+find . -name '*.log' -type f -delete
+find . -name '*.aux' -type f -delete
+find . -name '*.synctex.gz' -type f -delete
+find . -name '*.fls' -type f -delete
+find . -name '*.fdb_latexmk' -type f -delete
+
+tree > tree
