c34d08e497e7187925550ff3ca4265a511bb420d
ylf <iivodka5@gmail.com>
2019-08-05 12:28:35 +0000

    isse

--- clean.sh
+++
@@ -1,5 +0,0 @@
-find . -name '*.log' -type f -delete
-find . -name '*.aux' -type f -delete
-find . -name '*.synctex.gz' -type f -delete
-find . -name '*.fls' -type f -delete
-find . -name '*.fdb_latexmk' -type f -delete
